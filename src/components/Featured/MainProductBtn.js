ac9fe21afeaac491b7f7b6d74c6d5076fae06df1
EmadNazzal <emadnazzal95@gmail.com>
2022-10-18 18:15:18 +0300

    added extra features to products - created the QUICK VIEW btn

---
+++ src/components/Featured/MainProductBtn.js
@@ -0,0 +1,8 @@
+// import React from "react";
+// import "./MainProductBtn.css";
+
+// const MainProductBtn = () => {
+//   return <button className="MainProductBtn">QUICK VIEW</button>;
+// };
+
+// export default MainProductBtn;
--- src/components/Featured/Products.js
+++ src/components/Featured/Products.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { productsInfo } from "./ProductsData";
 import "./Products.css";
+// import MainProductBtn from "./MainProductBtn";
 export default class Products extends Component {
   render() {
     // const {id, img, title, price, isNew, country, currency} = productsInfo
@@ -11,6 +12,7 @@ export default class Products extends Component {
           return (
             <div className="cardHolder" key={item.id}>
               <img src={item.img} alt="Products" />
+              <button className="MainProductBtn">QUICK VIEW</button>
               <p>{item.title}</p>
               <small>{`${item.price} ${item.currency}`}</small>
             </div>
